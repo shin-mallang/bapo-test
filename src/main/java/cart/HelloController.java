6f1f36275bfcbe669780573efe3daa5a73a06154
shin-mallang <huipulci1@naver.com>
2023-05-28 02:07:04 +0900

    setting env

---
+++ src/main/java/cart/HelloController.java
@@ -0,0 +1,23 @@
+package cart;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${my-env.first}")
+    private String first;
+
+    @Value("${my-env.second}")
+    private String second;
+
+    @GetMapping("/hello")
+    String ping() {
+        return String.format(
+                "안녕! 설정된 정보는 FIRST = [$s], SECOND= [$s] !",
+                first,
+                second);
+    }
+}
