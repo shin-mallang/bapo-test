69a9b23676dcd6115fcfbd44fa5f17293613b843
shin-mallang <huipulci1@naver.com>
2023-06-04 00:17:18 +0900

    fail

--- src/test/java/cart/JwpCartApplicationTests.java
+++ src/test/java/cart/JwpCartApplicationTests.java
@@ -9,7 +9,7 @@ class JwpCartApplicationTests {
     @Test
     void contextLoads() {
         if (1 == 1) {
-            throw new IllegalArgumentException("1");
+            throw new IllegalArgumentException("21");
         }
     }
 
